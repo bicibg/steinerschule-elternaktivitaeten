fc5bca9d8538130d3ca15ebbd40fcb5fbaea519c
Bugra Ergin <bugra.ergin@hotelcard.com>
2025-09-18 00:55:59 +0200

    Add deletion reason modal to single delete actions on edit pages

--- app/Filament/Resources/CommentResource/Pages/EditComment.php
+++ app/Filament/Resources/CommentResource/Pages/EditComment.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\CommentResource\Pages;
 use App\Filament\Resources\CommentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms;
 
 class EditComment extends EditRecord
 {
@@ -13,7 +14,27 @@ class EditComment extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->requiresConfirmation()
+                ->modalHeading('Antwort löschen')
+                ->modalDescription('Bitte wählen Sie einen Grund für die Löschung.')
+                ->form([
+                    Forms\Components\Select::make('deletion_reason')
+                        ->label('Löschgrund')
+                        ->options([
+                            'year_archived' => 'Jahresarchivierung',
+                            'spam' => 'Spam',
+                            'inappropriate' => 'Unangemessen',
+                            'user_requested' => 'Auf Anfrage des Benutzers',
+                            'duplicate' => 'Duplikat',
+                        ])
+                        ->required(),
+                ])
+                ->before(function ($record, array $data): void {
+                    $record->deletion_reason = $data['deletion_reason'];
+                    $record->save();
+                })
+                ->visible(fn () => auth()->user()?->is_admin ?? false),
         ];
     }
 }
--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms;
 
 class EditPost extends EditRecord
 {
@@ -13,7 +14,27 @@ class EditPost extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->requiresConfirmation()
+                ->modalHeading('Forumbeitrag löschen')
+                ->modalDescription('Bitte wählen Sie einen Grund für die Löschung.')
+                ->form([
+                    Forms\Components\Select::make('deletion_reason')
+                        ->label('Löschgrund')
+                        ->options([
+                            'year_archived' => 'Jahresarchivierung',
+                            'spam' => 'Spam',
+                            'inappropriate' => 'Unangemessen',
+                            'user_requested' => 'Auf Anfrage des Benutzers',
+                            'duplicate' => 'Duplikat',
+                        ])
+                        ->required(),
+                ])
+                ->before(function ($record, array $data): void {
+                    $record->deletion_reason = $data['deletion_reason'];
+                    $record->save();
+                })
+                ->visible(fn () => auth()->user()?->is_admin ?? false),
         ];
     }
 }
